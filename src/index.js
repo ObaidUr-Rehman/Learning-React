cb3369081bbece8b32910c87013c58bc366d1f10
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-09-20 00:46:03 +0500

    ready

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+// ReactDOM.render(<><h1>Obaid Ur Rehman</h1>
+// <p>I am 19 years old</p>
+// <ol>
+//   <li>Money Heist</li>
+//   <li>Mirzapur</li>
+//   <li>Chat Lounge</li>
+//   <li>Om Shanti Om</li>
+//   <li>OOP Only</li>
+//   </ol>
+// </>,document.getElementById("root"));
+
+
+// DAYYYY 2
+
+// const cdate=new Date().toDateString();  //both day & date de rha hai
+// const ctime=new Date().toLocaleString();  //both date & time de rha hai
+// const etime=new Date().toLocaleTimeString(); // only time
+// const edate=new Date().toLocaleDateString(); // only date
+
+// ReactDOM.render(<><h1>Today Date is {cdate}</h1>
+// <h1>Today Dateeee is {ctime}</h1>
+// <h1>Today Dateeee is {etime}</h1>
+// <h1>Today Dateeee is {edate}</h1>
+// </>,document.getElementById("root"));
+
+const shoesimg = "https://api.lorem.space/image/shoes?w=150&h=150";
+const watchimg = "https://api.lorem.space/image/watch?w=150&h=150";
+const pizzaimg = "https://api.lorem.space/image/pizza?w=150&h=150";
+const gitLink  = "https://github.com/ObaidUr-Rehman?tab=overview&from=2022-09-01&to=2022-09-18";
+
+const heading = {
+  textAlign: "center",
+  color: "lightblue",
+  textShadow: "1px 2px brown",
+  display: "block",
+  fontWeight: "bold",
+  margin: "50px 0px"
+}
+
+const div = {
+  display:"flex",
+  justifyContent:"center",
+  alignItems:"center"
+}
+
+
+
+ReactDOM.render(<><h1 style={heading}>BEAUTIFUL IMAGES</h1>
+<p>
+  <div style={div} className="mydiv">
+  <img src={shoesimg} alt="shoesimage"/>
+  <img src={watchimg} alt="watchimage"/>
+  <a href={gitLink} target="_mygithub">
+  <img src={pizzaimg} alt="pizzaimage"/>
+  </a>
+  </div>
+</p>
+  </>,document.getElementById("root"));
+
