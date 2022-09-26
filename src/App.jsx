23edab7d0e96533608ac03bd98866d79f950f747
Obaid Ur Rehman <96888672+ObaidUr-Rehman@users.noreply.github.com>
2022-09-26 14:33:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import Heading from "./Components/Heading";
 import List from "./Components/List";
 import Para from "./Components/Para";
 
-function App(){
+const App = () => {
     return (
         <>
         <Heading></Heading>
-     <Para></Para>
-     <List></List>
+        <Para></Para>
+        <List></List>
         </>
     );
 }
-
 export default App;
