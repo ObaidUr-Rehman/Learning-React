beb7596c4a30940b643ff17b82f067778fb7e81b
Obaid Ur Rehman <96888672+ObaidUr-Rehman@users.noreply.github.com>
2022-09-30 21:18:26 +0500

    Update List.jsx

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -5,18 +5,17 @@ function List() {
         <>
         <ol>
             <li>I wake up early in the morning and offering Fajar Prayer.</li>
-            <li>I take shower.</li>
+            <li>I take shower. </li>
             <li>Then I get ready for University.</li>
             <li>I come back home at 4:30 pm.</li>
             <li>After come back i'm going to sleep for 2 hours.</li>
             <li>After 2 hours, I go out with friends to play Cricket.</li>
-            <li>At 8 I teach tutoring of Class IX and X.</li>
+            <li>At 7 I teach tutoring of Class IX and X.</li>
             <li>At 10 I usually have dinner with Family then I spend sometime on my Mobile.</li>
             <li>After that, I'm doing some work/assignment of University.</li>
-            <li>I soon fall to sleep and go to bed at 12:00.</li>
+            <li>I soon fall to sleep and go to bed at 12:00. </li>
         </ol>
         </>
     );
 }
-
 export default List;
